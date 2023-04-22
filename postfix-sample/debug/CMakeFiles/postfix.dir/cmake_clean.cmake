8d04a17d08678a6b3aa12cc7f7d84f5e63f63a81
Vitor Jordão <vitorpeviano@gmail.com>
2023-04-21 21:17:30 -0300

    Create first class files

---
+++ postfix-sample/debug/CMakeFiles/postfix.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/postfix.dir/parser.cpp.o"
+  "CMakeFiles/postfix.dir/parser.cpp.o.d"
+  "CMakeFiles/postfix.dir/postfix.cpp.o"
+  "CMakeFiles/postfix.dir/postfix.cpp.o.d"
+  "postfix"
+  "postfix.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/postfix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ start-environment.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+Sudo apt update
+sudo apt upgrade -y
+sudo apt install -y g++ gdb make cmake flex bison
