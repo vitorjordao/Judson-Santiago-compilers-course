7fdc2bbabe241a53f1051d92423dd4ca94b5c16f
Vitor Jordão <vitorpeviano@gmail.com>
2023-04-23 20:25:34 -0300

    Create my syntax analyser for prefix sintax

---
+++ Execices/lesson 5/pre-to-post-fixed/parser.cpp
@@ -0,0 +1,53 @@
+#include "parser.h"
+#include <iostream>
+#include <cctype>
+using std::cin;
+using std::cout;
+using std::endl;
+
+void Parser::Expr()
+{
+	
+	// expr -> + expr expr { print(+) }
+	if (lookahead == '+')
+	{
+		Match('+');
+		Expr();
+		Expr();
+		cout << '+';
+	}
+	// expr -> + expr expr { print(-) }
+	else if (lookahead == '-')
+	{
+		Match('-');
+		Expr();
+		Expr();
+		cout << '-';
+	} 
+	// expr -> term
+	else if(isdigit(lookahead)) {
+		cout << lookahead;
+		Match(lookahead);
+	}
+}
+
+void Parser::Match(char t)
+{
+	if (t == lookahead)
+		lookahead = cin.get();
+	else
+		throw SyntaxError{};
+}
+
+Parser::Parser()
+{
+	lookahead = 0; 
+}
+
+void Parser::Start()
+{
+	lookahead = cin.get();
+	Expr();
+	if (lookahead != '\n')
+		throw SyntaxError{};
+}
